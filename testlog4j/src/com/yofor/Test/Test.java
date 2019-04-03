96f1798e63016ada42f94e44dfdfb73461f6e59a
yofor <real_yofor@github.com>
2019-04-03 16:12:47 +0800

    v0.0.1

---
+++ testlog4j/src/com/yofor/Test/Test.java
@@ -0,0 +1,22 @@
+package com.yofor.Test;
+
+import org.apache.log4j.Logger;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Logger logger = Logger.getLogger(Test.class);
+		logger.debug("这是一个调试信息");
+		logger.info("普通信息1111111");
+		
+		try {
+			int i = 5/0;
+		} catch (Exception e) {
+			//e.printStackTrace();
+			logger.error(e.getMessage());
+		}
+		System.out.println("结束");
+	}
+	
+
+}
